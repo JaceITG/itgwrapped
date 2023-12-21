92d20b06e88116a1e80f9cf21c87b5a80100a409
Scheisse <jjwazoo@gmail.com>
2023-12-21 15:59:14 -0800

    Reduce XML data to yearly scores

--- src/components/xmlParser.jsx
+++ src/components/xmlParser.jsx
@@ -16,9 +16,74 @@ class XmlParser extends React.Component {
         this.readXML(props.stats);
     }
 
-    async parseStats(obj){
-        //Collect profile username
-        let profile = { username: obj["Stats"]["GeneralData"]["DisplayName"]["_text"] };
+    async parseStats(obj, year=2023){
+        // Collect profile username
+        let profile = {
+            username: obj["Stats"]["GeneralData"]["DisplayName"]["_text"],
+            scores: [],
+        };
+
+        // Get score data and filter to year
+        let scores = obj["Stats"]["SongScores"]["Song"];
+
+        // Filter through difficulties of each song, selecting only scores from correct year
+        let filteredScores = scores.flatMap((song) => {
+            let songName = song["_attributes"]["Dir"];
+
+            console.log("Processing song: " + songName);
+
+            let songScores = [];
+
+            // Pack Song Steps field into array in case it is an object (songs with 1 difficulty)
+            let diffArray = song["Steps"];
+            if (Array.isArray( diffArray )){
+                diffArray = diffArray;
+            }else{
+                diffArray = [ diffArray ];
+            }
+
+            // Add this year's scores from valid diffs to songScores
+            for ( const diffIndex in diffArray ){
+                let difficulty = diffArray[diffIndex];
+
+                // Ignore doubles
+                if ( difficulty["_attributes"]["StepsType"] != "dance-single" ) continue;
+
+                // Ignore charts last played before target year
+                let lastPlayedYear = parseInt( difficulty["HighScoreList"]["LastPlayed"]["_text"].split('-')[0] );
+                if ( lastPlayedYear < year ) continue;
+
+                // Ignore charts with no High Scores
+                if (difficulty["HighScoreList"]["HighScore"] === undefined ) continue;
+
+                let diffName = difficulty["_attributes"]["Difficulty"];
+
+                // Pack HighScores into array in case difficulty has only 1 score
+                let highScores = difficulty["HighScoreList"]["HighScore"];
+                if ( Array.isArray( highScores ) ){
+                    highScores = highScores;
+                }else{
+                    highScores = [ highScores ];
+                }
+                
+                for ( const scoreIndex in highScores ){
+                    let score = highScores[scoreIndex];
+
+                    // Check score date
+                    let scoreYear = parseInt( score["DateTime"]["_text"].split('-')[0] );
+                    if ( scoreYear != year ) continue;
+
+                    // Preserve song name/diff for standalone score object
+                    score.Song = { path: songName, difficulty: diffName }
+
+                    songScores.push(score);
+                }
+            }
+
+            return songScores;
+        });
+
+        profile.scores = filteredScores;
 
         this.setState({ profile: profile });
     }
@@ -46,10 +111,17 @@ class XmlParser extends React.Component {
             
             <div>
                 <div>Player: {profile.username}</div>
-                <JSONPretty id="json-pretty" data={data}></JSONPretty>
+                <div>Scores:</div>
+                <JSONPretty id="json-pretty" data={profile.scores}></JSONPretty>
             </div>
         );
     }
 }
+
+/* Full JSON Stats.xml debug element
+
+<JSONPretty id="json-pretty" data={data}></JSONPretty>
+
+*/
 
 export default XmlParser;
